ff619fc9bc22dd7efbbae55147a6a37f3fd90081
Prasi24bms017 <prasimishra2005@gmail.com>
2026-03-13 14:58:52 +0530

    Firebase setup complete

---
+++ src/firebase.ts
@@ -0,0 +1,31 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getDatabase } from "firebase/database";
+import { getStorage } from "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDmLwOcJLrUdfSoTQwUB0isZeRBhorcwjA",
+  authDomain: "safealert-india.firebaseapp.com",
+  projectId: "safealert-india",
+  storageBucket: "safealert-india.firebasestorage.app",
+  messagingSenderId: "251195067167",
+  appId: "1:251195067167:web:54dbb8a19ce0e3c2233e2a",
+  databaseURL: "https://safealert-india-default-rtdb.firebaseio.com"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getDatabase(app);
+export const storage = getStorage(app);
+
+export default app;
+```
+
+Press **Ctrl + S** to save!
+
+---
+
+Then in VS Code terminal run:
+```
+npm install firebase
